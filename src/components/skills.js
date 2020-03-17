cb9727e322ffce301b41fe946e606c74f0b2467e
Fernando Funez <fernandofunez@Fernandos-MacBook-Pro.local>
2020-03-17 16:56:30 -0400

    added icons to skill page

--- src/components/skills.js
+++ src/components/skills.js
@@ -1,8 +1,10 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { Footer } from "../components/footer.js";
-import { Progress } from "react-sweet-progress";
+import { Progress } from "antd";
+
 import "react-sweet-progress/lib/style.css";
+import "antd/dist/antd.css";
 
 export const Skills = () => {
   return (
@@ -19,39 +21,85 @@ export const Skills = () => {
         </Link>
       </div>
 
-      <div className="card-cont container mb-5">
-        <div className="skill-card1 card">
-          <img
-            src="https://i.imgur.com/jDkKGuD.png"
-            className="skill-card-img1 card-img-top img-fluid "
-            alt="..."
-          />
-          <div className="card-body text-center">
-            <Progress
-              type="circle"
-              strokeColor={{
-                "0%": "#FF0000",
-                "100%": "#FF0000"
-              }}
-              percent={70}
+      <div className="card-cont container d-flex justify-content-center mb-5">
+        <div className="row justify-content-center">
+          <div className="skill-card1 card  col-sm-2 col-lg-2">
+            <img
+              src="https://i.imgur.com/jDkKGuD.png"
+              className="skill-card-img1 card-img-top img-fluid "
+              alt="..."
             />
+            <div className="card-body text-center">
+              <Progress
+                className="progbar1"
+                type="circle"
+                strokeColor={{
+                  "0%": "#FF0000",
+                  "100%": "#FF0000"
+                }}
+                percent={70}
+                width={50}
+              />
+            </div>
           </div>
-        </div>
-      </div>
 
-      <div className="card-cont2 container text-center">
-        <div className="skill-card1 card">
-          <img
-            src="https://i.imgur.com/jDkKGuD.png"
-            className="card-img-top"
-            alt="..."
-          />
-          <div className="card-body text-center">
-            <h5 className="card-title">Card title</h5>
-            <p className="card-text">
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </p>
+          <div className="skill-card2 card ml-3  col-sm-2 col-lg-2">
+            <img
+              src="https://i.imgur.com/9tjOfI4.png"
+              className="skill-card-img1 card-img-top img-fluid "
+              alt="..."
+            />
+            <div className="card-body text-center">
+              <Progress
+                className="progbar1"
+                type="circle"
+                strokeColor={{
+                  "0%": "#FF0000",
+                  "100%": "#FF0000"
+                }}
+                percent={70}
+                width={50}
+              />
+            </div>
+          </div>
+          <div className="skill-card3 card ml-3  col-sm-2 col-lg-2">
+            <img
+              src="https://i.imgur.com/qG6T8bP.png"
+              className="skill-card-img1 card-img-top img-fluid "
+              alt="..."
+            />
+            <div className="card-body text-center">
+              <Progress
+                className="progbar1"
+                type="circle"
+                strokeColor={{
+                  "0%": "#FF0000",
+                  "100%": "#FF0000"
+                }}
+                percent={70}
+                width={50}
+              />
+            </div>
+          </div>
+
+          <div className="skill-card4 card ml-3  col-sm-2 col-lg-2">
+            <img
+              src="https://i.imgur.com/Cdzj37u.png"
+              className="skill-card-img1 card-img-top img-fluid "
+              alt="..."
+            />
+            <div className="card-body text-center">
+              <Progress
+                className="progbar1"
+                type="circle"
+                strokeColor={{
+                  "0%": "#FF0000",
+                  "100%": "#FF0000"
+                }}
+                percent={70}
+                width={50}
+              />
+            </div>
           </div>
         </div>
       </div>
